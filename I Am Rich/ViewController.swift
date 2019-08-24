56da30ed3008a9d20c41d36923e281c864fbd0c6
praveen <praveenj.pro@gmail.com>
2019-08-24 19:35:26 +0530

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Praveen J on 24/08/19.
+//  Copyright © 2019 Praveen J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
